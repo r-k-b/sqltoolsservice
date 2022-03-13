1a949aff0e5bc28dd8b7200cf806c2812b1f7d86
Robert K. Bell <robert.k.bell@gmail.com>
2022-03-13 15:47:39 +1100

    chore: fix up pathological casings
    Related: https://github.com/microsoft/sqltoolsservice/issues/886

--- shell.nix
+++ shell.nix
@@ -1,18 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
 let
   dotnetCombined = with pkgs.dotnetCorePackages;
-    combinePackages [ sdk_6_0 sdk_3_1 pkgs.dotnet-sdk ];
+    combinePackages [ sdk_3_1 pkgs.dotnet-sdk ];
 
 in pkgs.mkShell {
   name = "phdsysnet";
 
-  buildInputs = with pkgs; [
-    dotnetPackages.Nuget
-    dotnetCombined
-    mono6
-  ];
+  buildInputs = with pkgs; [ dotnetPackages.Nuget dotnetCombined mono6 ];
 
   shellHook = ''
-    echo '=== Tip: run nixfix to make the app(s) work better with nix ==='
+    echo Dependencies present.
   '';
 }
